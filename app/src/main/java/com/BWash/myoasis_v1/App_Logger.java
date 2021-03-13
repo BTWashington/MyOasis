1b2d191e022cd5b70c5af960dc07d3714e730cb3
BWash <BTWashington@student.fullsail.edu>
2021-03-13 14:11:40 -0500

    Initial Commit:
    Added Dependencies
    
    def lifecycle_version = "2.3.0"
        def arch_version = "2.1.0"
    
        implementation 'androidx.appcompat:appcompat:1.2.0'
        implementation 'com.google.android.material:material:1.3.0'
        implementation 'androidx.constraintlayout:constraintlayout:2.0.4'
        implementation "androidx.recyclerview:recyclerview:1.1.0"
        // For control over item selection of both touch and mouse driven selection
        implementation "androidx.recyclerview:recyclerview-selection:1.1.0"
        testImplementation 'junit:junit:4.+'
        androidTestImplementation 'androidx.test.ext:junit:1.1.2'
        androidTestImplementation 'androidx.test.espresso:espresso-core:3.3.0'
    
        // Lifecycle dependencies
        implementation "androidx.lifecycle:lifecycle-*:$lifecycle_version"
        implementation "androidx.lifecycle:lifecycle-common-java8:$lifecycle_version"
        implementation "androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version"
    
        // Livedata dependencies
        implementation "androidx.lifecycle:lifecycle-*:$lifecycle_version"
    
     Created App_Logger Class to log errors and help track down issues

---
+++ app/src/main/java/com/BWash/myoasis_v1/App_Logger.java
@@ -0,0 +1,145 @@
+package com.BWash.myoasis_v1;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.nio.file.NoSuchFileException;
+
+public class App_Logger {
+
+	private static final String MAIN_ACTIVITY_TAG = "Main Activity: ";
+	private static final String REGISTRATION_TAG = "Registration: ";
+	private static final String LOGIN_TAG = "Login: ";
+	private static final String PROFILE_VIEW_TAG = "Profile View: ";
+	private static final String Registration_Error = "Registration Error";
+	private static final String Profile_Error = "Profile Error";
+	private static final String Login_Error = "Login Error";
+	private static final String RUN_LOGGER = "Run Logger: ";
+	private static final String END_LOGGER = "End Logger";
+
+	private static final String ON_CREATE_METHOD = "onCreate: ";
+
+	// Logging Detail Variables
+	private int logID;
+	private String pageName;
+	private String methodName;
+	private String description;
+
+
+
+	// Constructors
+	public App_Logger(){}
+
+
+	public App_Logger(int logID, String pageName, String methodName, String description) {
+		this.logID = logID;
+		this.pageName = pageName;
+		this.methodName = methodName;
+		this.description = description;
+	}
+
+	// ToString Method
+	@Override
+	public String toString() {
+		return "App_Logger{" +
+				"logID=" + logID +
+				", pageName='" + pageName + '\'' +
+				", methodName='" + methodName + '\'' +
+				", description='" + description + '\'' +
+				'}';
+	}
+
+
+	// Class Methods
+	public void runLogger(Context context, String message, String TAG){
+		Log.d(TAG, RUN_LOGGER + context + ": " + message);
+
+	}
+
+	public void endLogger(Context context, String message, String TAG){
+		Log.d(TAG, END_LOGGER + ": " + context + ": " + message);
+	}
+
+	// Class Exception Toast Methods
+	public void errorMessage(Context context, String s){
+		Toast.makeText(context, s , Toast.LENGTH_SHORT).show();
+
+	}
+
+
+	// Getters and Setters
+
+
+	public static String getMainActivityTag() {
+		return MAIN_ACTIVITY_TAG;
+	}
+
+	public static String getRegistrationTag() {
+		return REGISTRATION_TAG;
+	}
+
+	public static String getLoginTag() {
+		return LOGIN_TAG;
+	}
+
+	public static String getProfileViewTag() {
+		return PROFILE_VIEW_TAG;
+	}
+
+	public static String getRegistration_Error() {
+		return Registration_Error;
+	}
+
+	public static String getProfile_Error() {
+		return Profile_Error;
+	}
+
+	public static String getLogin_Error() {
+		return Login_Error;
+	}
+
+	public static String getRunLogger() {
+		return RUN_LOGGER;
+	}
+
+	public static String getEndLogger() {
+		return END_LOGGER;
+	}
+
+	public static String getOnCreateMethod(){
+		return ON_CREATE_METHOD;
+	}
+
+	public int getLogID() {
+		return logID;
+	}
+
+	public void setLogID(int logID) {
+		this.logID = logID;
+	}
+
+	public String getPageName() {
+		return pageName;
+	}
+
+	public void setPageName(String pageName) {
+		this.pageName = pageName;
+	}
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
---
+++ app/src/main/java/com/BWash/myoasis_v1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.BWash.myoasis_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		App_Logger app_logger = new App_Logger();
+		app_logger.runLogger(MainActivity.this, App_Logger.getRunLogger(), App_Logger.getMainActivityTag());
+		app_logger.errorMessage(MainActivity.this, App_Logger.getOnCreateMethod());
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		app_logger.endLogger(MainActivity.this, App_Logger.getEndLogger(), App_Logger.getMainActivityTag());
+	}
+}
